463a4175312b6bcf0e937486257ee50035dd9af9
David <8584751@qq.com>
2017-12-10 18:39:27 +0800

    Hello World

---
+++ FirstTest/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by apple on 2017/12/10.
+//  Copyright © 2017年 HBU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
