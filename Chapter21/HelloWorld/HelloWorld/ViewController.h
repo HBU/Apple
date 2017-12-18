c230fcb32cbe319299f22d3381a0873a57525006
David <8584751@qq.com>
2017-12-18 18:27:46 +0800

    iOS learning

---
+++ Chapter21/HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by apple on 2017/12/18.
+//  Copyright © 2017年 HBU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong,nonatomic) IBOutlet UILabel *display;
+-(IBAction) click1;
+
+@end
+
---
+++ Chapter21/HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by apple on 2017/12/18.
+//  Copyright © 2017年 HBU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize display;
+
+-(IBAction) click1
+{
+    display.text = @"1";
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
